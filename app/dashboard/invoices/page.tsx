7d8c87e01b73f15a660ad0c0ab1d054855c7c91c
abhi62673 <abhiramragu51@gmail.com>
2025-02-27 22:16:43 +0530

    first commit

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,25 +1,37 @@
-import { fetchCardData } from "@/app/lib/data";
-import { Card } from "@/app/ui/dashboard/cards";
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
+ 
+  export default async function Page(props: {
+    searchParams?: Promise<{
+      query?: string;
+      page?: string;
+    }>;
+  }) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query);
 
-export default async function Page() {
-
-  const {
-      numberOfInvoices,
-      numberOfCustomers,
-      totalPaidInvoices,
-      totalPendingInvoices,
-    } = await fetchCardData();
-  return ( 
-          <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-          <Card title="Collected" value={totalPaidInvoices} type="collected" />
-          <Card title="Pending" value={totalPendingInvoices} type="pending" /> 
-          <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-          <Card
-            title="Total Customers"
-            value={numberOfCustomers}
-            type="customers"
-          />
-        </div>
-  )
-
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
